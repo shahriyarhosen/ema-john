24ec14f92b77992b07c5fbb10af3a2a4ac651808
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-03-31 19:25:28 +0600

    useCart hook

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import useCart from '../../hook/useCart';
 import useProducts from '../../hook/useProducts';
-import { addToDb, getStoreCart } from '../../utilities/fakedb';
+import { addToDb } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
 
 const Shop = () => {
-    const [cart, setCart] = useState([])
     const [products] = useProducts()
+    const [cart, setCart] = useCart(products)
 
     const addToCart = (selectedProduct) => {  
         console.log(selectedProduct);
@@ -25,21 +25,6 @@ const Shop = () => {
         setCart(newCart)
         addToDb(selectedProduct.id)
     }
-
-    useEffect(() => {
-      const storedCart = getStoreCart();
-      let saveCart = [];
-      for (const id in storedCart) {
-          const addedProduct = products.find((product) => product.id === id);
-          if (addedProduct) {
-              const quantity = storedCart[id];
-              addedProduct.quantity = quantity
-            saveCart.push(addedProduct)
-          }
-      }
-      setCart(saveCart);
-    }, [products])
-    
     return (
         <section className='shop-container'>
             <div className='products-container'>
--- src/hook/useCart.js
+++ src/hook/useCart.js
@@ -1,18 +1,23 @@
 import { useEffect, useState } from "react";
 import { getStoreCart } from "../utilities/fakedb";
 
-
 const useCart = (products) => {
-    const [cart, setCart] = useState([])
-    let saveCart = [];
-    useEffect(() => {
-        const storeCart = getStoreCart();
-        for (const id in storeCart) {
-            
-        }
-    }, [products])
-    
-    return
+  const [cart, setCart] = useState([]);
+  let saveCart = [];
+  useEffect(() => {
+    const storeCart = getStoreCart();
+    for (const id in storeCart) {
+      const addProduct = products.find((pd) => pd.id === id);
+      if (addProduct) {
+        const quantity = storeCart[id];
+        addProduct.quantity = quantity;
+        saveCart.push(addProduct);
+      }
+    }
+    setCart(saveCart);
+  }, [products]);
+
+  return [cart, setCart];
 };
 
 export default useCart;
