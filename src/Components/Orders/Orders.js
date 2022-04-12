3a27c17dad5e61aa0c3c6741637781213b28a45e
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-04-12 12:17:00 +0600

    review item details container all style add

--- src/Components/Orders/Orders.js
+++ src/Components/Orders/Orders.js
@@ -2,14 +2,21 @@ import React from "react";
 import useCart from "../../hook/useCart";
 import useProducts from "../../hook/useProducts";
 import Cart from "../Cart/Cart";
+import ReviewItem from "../ReviewItem/ReviewItem";
+import './Orders.css'
 
 const Orders = () => {
   const [products] = useProducts([]);
   const [cart] = useCart(products);
   return (
     <div className="shop-container">
-      <div className="products-container">
-          
+      <div className="review-item-container">
+        {cart.map((product) => (
+          <ReviewItem 
+          key={product.id} 
+          product={product}
+          ></ReviewItem>
+        ))}
       </div>
       <div className="cart-container">
         <Cart cart={cart}></Cart>
--- src/Components/ReviewItem/ReviewItem.js
+++ src/Components/ReviewItem/ReviewItem.js
@@ -1,9 +1,37 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
+import "./ReviewItem.css";
 
-const ReviewItem = () => {
+const ReviewItem = ({ product }) => {
+  const { name, img, price, shipping, quantity } = product;
   return (
-    <div>
-      <h1>this is review item page</h1>
+    <div className="review-item">
+      <div>
+        <img src={img} alt={name} />
+      </div>
+      <div className="review-item-details-container ">
+        <div className="review-item-details">
+          <p>{name}</p>
+          <p>
+            Price: $<span className="orange-color">{price}</span>
+          </p>
+          <p>
+            <small>Shipping: {shipping}</small>
+          </p>
+          <p>
+            <small>Quantity: {quantity}</small>
+          </p>
+        </div>
+        <div className="delete-container">
+          <button className="delete-btn">
+            <FontAwesomeIcon
+              className="delete-icon"
+              icon={faTrashAlt}
+            ></FontAwesomeIcon>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
