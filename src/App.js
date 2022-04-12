45f2b07bd560f0cfabdf2ece68547c348fc16dfe
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-04-12 17:06:03 +0600

    login signup compo add & route setup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Header from './Components/Header/Header';
-import Inventory from './Components/Inventory/Inventory';
-import Orders from './Components/Orders/Orders';
-import Shop from './Components/Shop/Shop';
-import About from './Components/About/About';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./Components/Header/Header";
+import Inventory from "./Components/Inventory/Inventory";
+import Orders from "./Components/Orders/Orders";
+import Shop from "./Components/Shop/Shop";
+import About from "./Components/About/About";
+import Login from "./Components/Login/Login";
+import Signup from "./Components/Signup/Signup";
 
 function App() {
   return (
     <div>
       <Header></Header>
       <Routes>
-        <Route path='/' element={<Shop></Shop>}></Route>
-        <Route path='/shop' element={<Shop></Shop>}></Route>
-        <Route path='/orders' element={<Orders></Orders>}></Route>
-        <Route path='/inventory' element={<Inventory></Inventory>}></Route>
-        <Route path='/about' element={<About></About>}></Route>
+        <Route path="/" element={<Shop />}></Route>
+        <Route path="/shop" element={<Shop />}></Route>
+        <Route path="/orders" element={<Orders />}></Route>
+        <Route path="/inventory" element={<Inventory />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
       </Routes>
     </div>
   );
---
+++ src/Components/Signup/Signup.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Signup = () => {
+    return (
+        <div>
+            This is Sign up page
+        </div>
+    );
+};
+
+export default Signup;
