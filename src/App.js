77abd50d4a97b3ff9963d1acc4d0094343b940ae
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-04-13 00:43:36 +0600

    RequireAuth add

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Shop from "./Components/Shop/Shop";
 import About from "./Components/About/About";
 import Login from "./Components/Login/Login";
 import Signup from "./Components/Signup/Signup";
+import RequireAuth from "./Components/RequireAuth/RequireAuth";
+import Shipment from "./Components/Shipment/Shipment";
 
 function App() {
   return (
@@ -16,7 +18,22 @@ function App() {
         <Route path="/" element={<Shop />}></Route>
         <Route path="/shop" element={<Shop />}></Route>
         <Route path="/orders" element={<Orders />}></Route>
-        <Route path="/inventory" element={<Inventory />}></Route>
+        <Route
+          path="/inventory"
+          element={
+            <RequireAuth>
+              <Inventory />
+            </RequireAuth>
+          }
+        ></Route>
+        <Route
+          path="/shipment"
+          element={
+            <RequireAuth>
+              <Shipment />
+            </RequireAuth>
+          }
+        ></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/signup" element={<Signup />}></Route>
---
+++ src/Components/RequireAuth/RequireAuth.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../Firebase/Firebase.init';
+
+const RequireAuth = ({ children }) => {
+  const [user] = useAuthState(auth);
+  let location = useLocation();
+
+  if (!user) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+  
+  return children;
+};
+
+export default RequireAuth;
